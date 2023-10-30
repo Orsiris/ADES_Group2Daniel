df21c7c1bae1b15e4de61aa431dbcce12bac33c0
Orsiris <DANIELWONG.22@ichat.sp.edu.sg>
2023-10-31 01:25:51 +0800

    done scrolling text

--- frontend/public/homepage/main.js
+++ frontend/public/homepage/main.js
@@ -2,7 +2,8 @@ new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
     perView: 1,
-    animationDuration: 500,
+    animationDuration: 1000,
+    
     // Add pagination
     pagination: {
         el: '.glide__bullets',
